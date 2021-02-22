c0a01f050fe341b2aab3b84534c7491e444b5f0e
Anbumani <anbuathypathy@gmail.com>
2021-02-22 20:34:01 +0530

    Add you firebase config

--- firebase/fire.js
+++ firebase/fire.js
@@ -1,13 +1,8 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC7HZ2WGEaMMIeg_68fDEfY_GM-RHUZXBw",
-    authDomain: "auth-demo-4e909.firebaseapp.com",
-    projectId: "auth-demo-4e909",
-    storageBucket: "auth-demo-4e909.appspot.com",
-    messagingSenderId: "758786712936",
-    appId: "1:758786712936:web:f2a7c401980ba7e4e6e2dd",
-    measurementId: "G-N1S1YXE10Q"
+    /* Add your Firebase configuration here
+*/
   };
 
 if(!firebase.apps.length){
